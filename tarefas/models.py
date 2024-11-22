4963f5bc9793bec181dccdf3ddbb9b1f73448dd6
Jeison SR <jeison.sr.cursos@gmail.com>
2024-11-22 16:20:42 -0300

    Adição de comentários no model Tarefa.

--- tarefas/models.py
+++ tarefas/models.py
@@ -3,9 +3,18 @@ from uuid import uuid4
 from django.db import models
 
 
+"""
+Função para renomear uma foto adicionada a aplicação.
+Ela será utilizada no campo foto do model Tarefa.
+"""
 def upload_foto(instance, filename):
     return f"{uuid4}-{filename}"
 
+"""
+Model resposnsável pela persistência de dados.
+Sua função é servir como um meio de comunicação entre
+a aplicação e a tabela tarefa do banco de dados.
+"""
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=60, null=False)
     descricao = models.CharField(max_length=120)
